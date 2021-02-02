49135cf5cb636d433edf195335517acbd7cd2dd3
Bysandy <leeonid@gmil.com>
2021-02-02 09:28:23 +0300

    add PracticalFormTests

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -3,9 +3,10 @@ package tests;
 import com.codeborne.selenide.ClickOptions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
 
@@ -21,14 +22,16 @@ public class PracticeFormTests {
         $("#gender-radio-1").click(ClickOptions.usingDefaultMethod());
         $("#userNumber").setValue("1234567890");
         // Date of Birth
-        //Subjects
-
+        $("#subjectsInput").val("Chemistry").pressEnter();
         $("#hobbies-checkbox-1").click(ClickOptions.usingJavaScript());
         $("#hobbies-checkbox-2").click(ClickOptions.usingJavaScript());
         $("#hobbies-checkbox-3").click(ClickOptions.usingJavaScript());
-        //Picture
+        $("#uploadPicture").uploadFile(new File("/Users/user/Downloads/IMG_1285.jpg"));
         $("#currentAddress").setValue("Very very long address for this test");
-        //State and City
+        $("#react-select-3-input").val("NCR").pressEnter();
+        $("#react-select-4-input").val("Gurgaon").pressEnter();
+        sleep(200);
+
 
         $("#submit").click();
 
